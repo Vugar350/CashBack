4b7188cb24c50dd758639419f4b57acc1e5869da
vugar350 <Vugar350@mail.ru>
2022-02-13 06:23:19 +0300

    initial commit

--- src/main/java/ru/Netology/Service/CashbackHackService.java
+++ src/main/java/ru/Netology/Service/CashbackHackService.java
@@ -1,6 +1,7 @@
 package ru.Netology.Service;
 
-public class CashbackHackService {private final int boundary = 1000;
+public class CashbackHackService {
+    private final int boundary = 1000;
 
     public int remain(int amount) {
         return boundary - amount % boundary;
